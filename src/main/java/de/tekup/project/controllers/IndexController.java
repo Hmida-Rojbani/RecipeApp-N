7f27482c78e79c57028e5733f3f3ccbd03b6b80c
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-12-07 21:36:32 +0100

    TODO close #4

---
+++ src/main/java/de/tekup/project/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package de.tekup.project.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+	
+	// call service 
+	// to get all Recipes
+	
+	// add recipes to model of index.html
+
+}
