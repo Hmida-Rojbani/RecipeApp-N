7c1d1915c0bd3db5353a9994ae6d9a6ba5761d38
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-12-21 19:02:35 +0100

    add data to show html. close #9.

--- src/main/java/de/tekup/project/controllers/IngredientController.java
+++ src/main/java/de/tekup/project/controllers/IngredientController.java
@@ -1,18 +1,23 @@
 package de.tekup.project.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import de.tekup.project.services.RecipeService;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
 public class IngredientController {
 	
+	private RecipeService recipeService;
 	
 	@GetMapping("/recipe/{recipeId}/ingredients")
-	public String showIngredients() {
-		
+	public String showIngredients(@PathVariable("recipeId") long recipeId, Model model) {
+		model.addAttribute("recipeId", recipeId);
+		model.addAttribute("ingredients", recipeService.getRecipeById(recipeId).getIngredients());
 		return "recipe/ingredient/show";
 	}
 
